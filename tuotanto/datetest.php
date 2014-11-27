5a82a49d5d9d49d441a7095a2e5802f8924ccb95
Vili Lang <lang.vili@gmail.com>
2014-11-27 13:13:12 +0200

    datetest

---
+++ tuotanto/datetest.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>test</title>
+</head>
+<body>
+<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+
+    <script type="text/javascript">
+    var test = '<? print date("Y/n/d/H/i/s")?>';
+        $(document).ready(function () {
+
+            alert(test);
+
+        });
+
+    </script>
+</body>
+</html>
